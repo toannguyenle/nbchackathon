d69d64af6133716aca3afd6bf7a6ec401c8ea2e8
toannguyenle <toan@me.com>
2014-11-16 11:26:38 -0800

    bootstrap complete

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,9 @@ Fandangoapp2::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+    # ALLOW IFRAME
+  config.action_dispatch.default_headers = {
+    'X-Frame-Options' => 'ALLOWALL'
+  }
 end
